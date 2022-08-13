43e7351dcf31bade9ec74a0b21d7266ee28fa571
Jaimefilho <jaime.osmar.rodrigues.filho@gmail.com>
2022-08-13 16:36:31 +0000

    apache v,1.0

---
+++ script-iac2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Atualizando o Servidor ..."
+apt-get update -y
+apt-get upgrade -y
+
+echo "instalando apache e o unzip ..."
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "baixando e copiando os arquivos da aplicação ..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
